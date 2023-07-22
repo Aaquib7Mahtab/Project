0282638b99f35aeb99b484634a9607f0424cd615
Aaquib <aaquib7mahtab786@gmail.com>
2023-07-23 02:18:49 +0530

    final commit

--- script.js
+++ script.js
@@ -2,94 +2,77 @@ var hr = 0;
 var min = 0;
 var sec = 0;
 var count = 0;
-
 var timer = false;
 
 function $id(id) {
     return document.getElementById(id);
 }
 
-function start() {
+function toggleTimer() {
+    if (timer) {
+        stopTimer();
+    } else {
+        startTimer();
+    }
+}
+
+function startTimer() {
     timer = true;
-    document.getElementById("start").disabled=true;
+    $id("start").innerText = 'Pause';
     stopwatch();
 }
 
-function stop() {
+function stopTimer() {
     timer = false;
-    document.getElementById("start").disabled=false;
-
+    $id("start").innerText = 'Start';
 }
 
-function reset() {
-    timer = false;
-
-    hr = 0;
-    min = 0;
-    sec = 0;
-    count = 0;
-
-    $id("hr").innerHTML = "00";
-    $id("min").innerHTML = "00";
-    $id("sec").innerHTML = "00";
-    $id("count").innerHTML = "00";
-
+function resetTimer() {
+    stopTimer();
+    hr = min = sec = count = 0;
+    updateDisplay();
+    clearLaps();
 }
 
 function stopwatch() {
-    if (timer == true)
-        count = count + 1;
+    if (timer) {
+        count = (count + 1) % 100;
 
-    if (count == 99) {
-        sec = sec + 1;
-        count = 0;
-    }
-    if (sec == 59) {
-        min = min + 1;
-        sec = 0;
-    }
-    if (min == 59) {
-        hr = hr + 1;
-        min = 0;
-        sec = 0;
-    }
+        if (count === 0) {
+            sec = (sec + 1) % 60;
 
-    var hrString = hr;
-    var minString = min;
-    var secString = sec;
-    var countString = count;
+            if (sec === 0) {
+                min = (min + 1) % 60;
 
-    if (hr < 10) {
-        hrString = "0" + hrString;
-    }
-    if (min < 10) {
-        minString = "0" + minString;
-    }
-    if (sec < 10) {
-        secString = "0" + secString;
-    }
-    if (count < 10) {
-        countString = "0" + countString;
-    }
+                if (min === 0) {
+                    hr = (hr + 1) % 100;
+                }
+            }
+        }
 
-    $id("hr").innerHTML = hrString;
-    $id("min").innerHTML = minString;
-    $id("sec").innerHTML = secString;
-    $id("count").innerHTML = countString;
-    setTimeout("stopwatch()", 10)
+        updateDisplay();
+        setTimeout(stopwatch, 10);
+    }
+}
+
+function updateDisplay() {
+    $id("hr").innerText = formatTime(hr);
+    $id("min").innerText = formatTime(min);
+    $id("sec").innerText = formatTime(sec);
+    $id("count").innerText = formatTime(count);
+}
+
+function formatTime(time) {
+    return time.toString().padStart(2, '0');
 }
 
 function lap() {
-    console.log(hr, min, sec, count)
-    var Laps = $id('laps');
-    Laps.innerHTML += "<li>" + hr + ":" + min + ":" + sec + ":" + count + "</li>";
+    var lapsList = $id('laps');
+    var lapItem = document.createElement('li');
+    lapItem.innerText = `${formatTime(hr)}:${formatTime(min)}:${formatTime(sec)}:${formatTime(count)}`;
+    lapsList.appendChild(lapItem);
 }
 
-function clearLap() {
-    $id('laps').remove();
-}
-
-function getLocalTime(){
-    const d = new Date().toLocaleTimeString();
-    console.log(d);
+function clearLaps() {
+    $id('laps').innerHTML = '';
 }
